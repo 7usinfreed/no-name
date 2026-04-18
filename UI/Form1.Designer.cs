f8df3ea74a2bbdb1579c287de5c744cfb9f47e1f
7usinfreed <frydsuin@gmail.com>
2026-04-18 13:56:07 +0300

    Add project files.

---
+++ UI/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelbtn = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelhome = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelbtn.SuspendLayout();
+            this.panelhome.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelbtn
+            // 
+            this.panelbtn.BackColor = System.Drawing.Color.Teal;
+            this.panelbtn.Controls.Add(this.panel2);
+            this.panelbtn.Controls.Add(this.button5);
+            this.panelbtn.Controls.Add(this.button4);
+            this.panelbtn.Controls.Add(this.button2);
+            this.panelbtn.Controls.Add(this.button1);
+            this.panelbtn.Location = new System.Drawing.Point(0, 99);
+            this.panelbtn.Name = "panelbtn";
+            this.panelbtn.Size = new System.Drawing.Size(234, 626);
+            this.panelbtn.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(243, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1247, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button5.Location = new System.Drawing.Point(-7, 188);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(244, 68);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "button1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.Location = new System.Drawing.Point(-7, 128);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(244, 65);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "button1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(-7, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(244, 64);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(-7, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panelhome
+            // 
+            this.panelhome.BackColor = System.Drawing.Color.Teal;
+            this.panelhome.Controls.Add(this.label1);
+            this.panelhome.Location = new System.Drawing.Point(237, 0);
+            this.panelhome.Name = "panelhome";
+            this.panelhome.Size = new System.Drawing.Size(1253, 100);
+            this.panelhome.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(519, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HOME";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(237, 100);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(42)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(1487, 725);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelhome);
+            this.Controls.Add(this.panelbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelbtn.ResumeLayout(false);
+            this.panelhome.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelbtn;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelhome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ UI/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            panelhome.BackColor = Color.Teal;
+            panel1.BackColor = Color.DarkSlateGray;
+            button2.BackColor = Color.Teal;
+           
+            button4.BackColor = Color.Teal;
+            button5.BackColor = Color.Teal;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panelhome.BackColor = Color.Red;
+            panel1.BackColor = Color.DarkRed;
+            button2.BackColor = Color.Red;
+            button4.BackColor = Color.Teal;
+            button5.BackColor = Color.Teal;
+
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            panelhome.BackColor = Color.Blue;
+            panel1.BackColor = Color.DarkBlue;
+            button4.BackColor = Color.Blue;
+            button2.BackColor = Color.Teal;
+            button5.BackColor = Color.Teal;
+
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            panelhome.BackColor = Color.Green;
+            panel1.BackColor = Color.DarkGreen;
+            button4.BackColor = Color.Teal;
+            button2.BackColor = Color.Teal;
+            button5.BackColor = Color.Green;
+        }
+    }
+}
