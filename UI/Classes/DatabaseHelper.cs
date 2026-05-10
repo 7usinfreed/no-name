c0a5c4e8ebda1a4c258c9bcad10bae7fc310132b
I-O01 <abdal-rhaman200@hotmail.com>
2026-05-10 13:54:36 +0400

    اضفت كلاس للربط ب الجريد

---
+++ UI/Classes/DatabaseHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Classes
+{
+   
+       public class DatabaseHelper
+        {
+            // نص الاتصال - يفضل وضعه هنا لتغييره من مكان واحد فقط
+            private string connectionString = @"Server=.; Database=Mangment; Integrated Security=True;";
+
+            // دالة لجلب البيانات (تستخدم للـ Dashboard والـ DataGridView)
+            public DataTable GetDataTable(string query)
+            {
+                DataTable dt = new DataTable();
+                try
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                        adapter.Fill(dt);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("خطأ في جلب البيانات: " + ex.Message);
+                }
+                return dt;
+            }
+
+            // دالة لتنفيذ الأوامر (Insert, Update, Delete)
+            public bool ExecuteCommand(string query)
+            {
+                try
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        return cmd.ExecuteNonQuery() > 0;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("خطأ في تنفيذ الأمر: " + ex.Message);
+                    return false;
+                }
+            }
+       }
+    
+}
